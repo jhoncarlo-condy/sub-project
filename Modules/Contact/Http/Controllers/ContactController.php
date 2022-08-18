fce35e5664e57b2967fd0d8c8865bed9e01983fa
condy <jhoncarlo.condy@davidvenneit.com>
2022-08-18 23:32:52 +0800

    update

--- Modules/Contact/Http/Controllers/ContactController.php
+++ Modules/Contact/Http/Controllers/ContactController.php
@@ -9,10 +9,7 @@ use Modules\Contact\Http\Requests\CreateContactRequest;
 
 class ContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
+
     public function index()
     {
         return response()->json([
@@ -20,21 +17,11 @@ class ContactController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
     public function store(CreateContactRequest $request)
     {
-        dd($request);
+        dd('store');
     }
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
     public function show($id)
     {
         return response()->json([
@@ -42,22 +29,11 @@ class ContactController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
     public function update(Request $request, $id)
     {
-        //
+        dd('update');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
     public function destroy($id)
     {
         return response()->json([
